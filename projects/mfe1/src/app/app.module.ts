ef0f0afd87214bf0ca856d21d7959914399dfe55
Paulina Sikacka <paulina.sikacka@convista.com>
2024-08-06 14:54:12 +0200

    3. prepare mfe feature-modules with shell template

--- projects/mfe1/src/app/app.module.ts
+++ projects/mfe1/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FeatureAModule } from './feature-a/feature-a.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FeatureAModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projects/mfe2/src/app/app.module.ts
+++ projects/mfe2/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FeatureBModule } from './feature-b/feature-b.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FeatureBModule
   ],
   providers: [],
   bootstrap: [AppComponent]
